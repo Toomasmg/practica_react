eec87a803bdf91b7d1e90793ab8534450ed21965
mateo gomez <mateogomez3521@gmail.com>
2026-05-19 19:41:34 -0300

    add auth and cart providers

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { CounterProvider } from './contexts/CounterContext';
+import { AuthProvider } from './contexts/AuthContext';
+import { CartProvider } from './contexts/CartContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -10,22 +12,25 @@ import Products from './pages/Products';
 import Cart from './pages/Cart';
 import './App.css';
 
-
 export default function App() {
   return (
     <ThemeProvider>
       <LanguageProvider>
         <CounterProvider>
-          <BrowserRouter>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/cart" element={<Cart />} />
-            </Routes>
-            <Footer />
-          </BrowserRouter>
+          <AuthProvider>
+            <CartProvider>
+              <BrowserRouter>
+                <Navbar />
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/products" element={<Products />} />
+                  <Route path="/cart" element={<Cart />} />
+                </Routes>
+                <Footer />
+              </BrowserRouter>
+            </CartProvider>
+          </AuthProvider>
         </CounterProvider>
       </LanguageProvider>
     </ThemeProvider>
